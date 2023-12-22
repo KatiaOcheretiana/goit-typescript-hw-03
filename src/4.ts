7ca6a34502151090a7dbe8400818569b31c6f94f
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-12-22 11:11:54 +0200

    completed

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  public tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Come in!");
+    } else {
+      console.log("You can not come in.");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open. Come in.");
+    } else {
+      console.log("Door is close, try to find the rigth key");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
